14f076a9827d84bf675d89d9f144b18d33ac4a93
psychobunny <psycho.bunny@hotmail.com>
2013-11-18 14:11:07 -0500

    tyvm stackoverflow (Y)

--- library.js
+++ library.js
@@ -5,6 +5,7 @@
 		embed = '<iframe width="640" height="360" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>';
 
 	Youtube.parse = function(postContent, callback) {
+		// http://stackoverflow.com/questions/7168987/
 		postContent = postContent.replace(/<a href="(?:http:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)<\/a>/g, embed);
 		callback(null, postContent);
 	};
