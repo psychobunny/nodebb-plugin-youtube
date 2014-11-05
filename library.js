8fbf0144aba3f7cf5775c246d512bd16c3fe900e
Barış Soner Uşaklı <barisusakli@gmail.com>
2014-11-05 17:42:44 -0500

    update for new hook
    dont create regexes everytime parse is called.
    data has postData and uid

--- library.js
+++ library.js
@@ -4,23 +4,26 @@
 	var Youtube = {},
 		embed = '<div class="video-container"><iframe class="youtube-plugin" src="//www.youtube.com/embed/$1?wmode=opaque" allowfullscreen></iframe></div>';
 
-	Youtube.parse = function(postContent, callback) {
-		// modified from http://stackoverflow.com/questions/7168987/
-		var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">.+<\/a>/g;
-		var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">.+<\/a>/g;
-		var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+)">.+<\/a>/;
+	// modified from http://stackoverflow.com/questions/7168987/
+	var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com)\/(?:watch\?v=)(.+)">.+<\/a>/g;
+	var	shortUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:youtu\.be)\/(.+)">.+<\/a>/g;
+	var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)youtube.com\/embed\/([\w\-_]+)">.+<\/a>/;
 
-		if (postContent.match(embedUrl)) {
-			postContent = postContent.replace(embedUrl, embed);
+	Youtube.parse = function(data, callback) {
+		if (!data || !data.postData || !data.postData.content) {
+			return callback(null, data);
 		}
-		if (postContent.match(regularUrl)) {
-			postContent = postContent.replace(regularUrl, embed);
+		if (data.postData.content.match(embedUrl)) {
+			data.postData.content = data.postData.content.replace(embedUrl, embed);
 		}
-		if (postContent.match(shortUrl)) {
-			postContent = postContent.replace(shortUrl, embed);
+		if (data.postData.content.match(regularUrl)) {
+			data.postData.content = data.postData.content.replace(regularUrl, embed);
+		}
+		if (data.postData.content.match(shortUrl)) {
+			data.postData.content = data.postData.content.replace(shortUrl, embed);
 		}
 
-		callback(null, postContent);
+		callback(null, data);
 	};
 
 	module.exports = Youtube;
